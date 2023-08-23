337fae454b47e17d285f4131d2f9aeb3ab0b4b95
vincentmanz <vincent.manzanilla@gmail.com>
2023-08-23 16:58:28 +0200

    trying to count the number of missing data

--- server_general_stats.R
+++ server_general_stats.R
@@ -12,8 +12,8 @@ server_general_stats <- function(input, output, session) {
     updateSelectInput(session, 'var2', choices = c("select" = "", colnames(df)))
   })
   
-  # Observer for the 'Run' button 
-  observeEvent(input$run, {
+  # Observer for the 'Run' button for variables
+  observeEvent(input$run_var, {
     req(df_reactive(), input$var1, input$var2)
     df <- df_reactive()
     
@@ -30,4 +30,61 @@ server_general_stats <- function(input, output, session) {
       })
     }
   })
+  
+  # Observer for the 'Run' button for column ranges
+  observeEvent(input$run_col_ranges, {
+    req(df_reactive(), input$col_ranges)
+    df <- df_reactive()
+    
+    col_ranges <- unlist(strsplit(input$col_ranges, ","))
+    
+    missing_percentages <- numeric(length(col_ranges))
+    missing_counts <- integer(length(col_ranges))
+    
+    for (i in seq_along(col_ranges)) {
+      range_str <- trimws(col_ranges[i])
+      if (grepl("-", range_str)) {
+        range_parts <- as.numeric(strsplit(range_str, "-")[[1]])
+        if (length(range_parts) == 2 && all(!is.na(range_parts))) {
+          start_col <- range_parts[1]
+          end_col <- range_parts[2]
+          
+          num_zeros_nas <- sum(df[start_col:end_col] == 0 | is.na(df[start_col:end_col]))
+          total_cells <- length(df[start_col:end_col])
+          missing_percentage <- (num_zeros_nas / total_cells) * 100
+          
+          missing_percentages[i] <- missing_percentage
+          missing_counts[i] <- num_zeros_nas
+        }
+      } else if (grepl(":", range_str)) {
+        range_parts <- as.numeric(strsplit(range_str, ":")[[1]])
+        if (length(range_parts) == 2 && all(!is.na(range_parts))) {
+          start_col <- range_parts[1]
+          end_col <- range_parts[2]
+          
+          num_zeros_nas <- sum(df[start_col:end_col] == 0 | is.na(df[start_col:end_col]))
+          total_cells <- length(df[start_col:end_col])
+          missing_percentage <- (num_zeros_nas / total_cells) * 100
+          
+          missing_percentages[i] <- missing_percentage
+          missing_counts[i] <- num_zeros_nas
+        }
+      }
+    }
+    
+    output$missing_info <- renderUI({  
+      result_table <- data.frame(
+        Column_Range = col_ranges,
+        Missing_Count = missing_counts,
+        Missing_Percentage = missing_percentages
+      )
+      
+      table_html <- renderTable(result_table, class = 'table shiny-table table-spacing-s', sanitize.text.function = function(x) x)
+      
+      HTML(as.character(table_html))
+    })
+  })
+  
+#  outputOptions(output, "missing_info", suspendWhenHidden = FALSE)  # Keep output active
+  
 }
--- ui_general_stats.R
+++ ui_general_stats.R
@@ -4,10 +4,14 @@ generateGeneralStatsUI <- function() {
     sidebarPanel(
       selectInput('var1', 'Population', choices = NULL),
       selectInput('var2', 'Individuals', choices = NULL),
-      actionButton("run", "Run")
+      actionButton("run_var", "Run"),
+      hr(),
+      textInput('col_ranges', 'Select Genotypes (e.g., 1-4, 5:10)'),
+      actionButton("run_col_ranges", "Run")
     ),
     mainPanel(
-      tableOutput("count_table")
+      tableOutput("count_table"),
+      verbatimTextOutput("missing_info")
       )
   )
 }
