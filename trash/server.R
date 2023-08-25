973267751b14f492b812ddb9e9f3790db6f5e3c7
vincentmanz <vincent.manzanilla@gmail.com>
2023-08-25 16:47:24 +0200

    simply code

---
+++ trash/server.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(plotly)
+library(dplyr)
+library(shinyalert)
+
+
+source("server_import_data.R")
+source("server_general_stats.R")
+#source("server_map.R")
+source("server_genetic_drift.R")
+
