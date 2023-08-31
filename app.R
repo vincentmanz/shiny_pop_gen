a83419a6f6174b9ab4cf07a541782076df1e26f7
vincentmanz <vincent.manzanilla@gmail.com>
2023-08-31 16:37:48 +0200

    updated splash screen.
    started the general stats

--- app.R
+++ app.R
@@ -9,12 +9,17 @@ library(shinydashboard)
 library(dplyr)
 library(shinyalert)
 library(shinydashboardPlus)
+library(hierfstat)
+library(kableExtra)
 
 # Source server and UI files
 source("server_import_data.R")
 source("server_genetic_drift.R")
 source("ui_import_data.R")
 source("ui_genetic_drift.R")
+source("ui_general_stats.R")
+source("server_general_stat.R")
+
 
 shiny.react::enableReactDebugMode()
 
@@ -22,16 +27,43 @@ linebreaks <- function(n) {
   HTML(strrep(br(), n))
 }
 
-## dashboard version
+## dashboard layout
 header <- dashboardHeader(title = "GenoPop")
 sidebar <- dashboardSidebar(
   sidebarMenu(id="sidebar",  
               menuItem("Welcome to GenoPop", tabName = "welcome", icon = icon("home"),  selected=TRUE),
-              menuItem("Data import and filtering", tabName = "data", icon = icon("dashboard")),
-              menuItem("Genetic drift", tabName = "drift", icon = icon("dashboard"))
+              menuItem("Data import and Filtering", tabName = "data", icon = icon("dashboard")),
+              menuItem("General Statsistics", tabName = "general_stats", icon = icon("dashboard")),
+              menuItem("Genetic Drift", tabName = "drift", icon = icon("dashboard"))
   )
 )
 
+# Create a data frame for the one example
+
+data_one_col <- data.frame(
+  Population = c("Boulouparis", "Boulouparis", "Gadji", "PortLaguerre", "Sarramea"),
+  B12 = c("192/194", "200/200", "0/0", "145/145", "0/0"),
+  C07 = c("145/192", "179/179", "92/100", "92/92", "92/92")
+)
+# Create a data frame for the two-column example
+data_two_col <- data.frame(
+  Population = c("Boulouparis", "Gadji", "PortLaguerre", "Sarramea"),
+  B12 = c(192, 200, 0, 145),
+  B12_2 = c(194, 200, 0, 145),
+  C07 = c(145, 179, 92, 92),
+  C07_2 = c(192, 179, 92, 100)
+)
+
+# Create a data frame for the latitude/longitude example
+data_gps <- data.frame(
+  Population = c("Boulouparis", "Gadji", "PortLaguerre", "Sarramea"),
+  Latitude = c(-21.86444444, -22.16805556, -22.10111111, -21.64111111),
+  Longitude = c(166.0391667, 166.2694444, 166.3030556, 165.8461111),
+  B12 = c(192, 200, 0, 145),
+  "B12" = c(194, 200, 0, 145),
+  C07 = c(145, 179, 92, 92),
+  "C07" = c(192, 179, 92, 100)
+)
 
 body <- dashboardBody(
   tabItems(
@@ -45,10 +77,50 @@ body <- dashboardBody(
           "For more comprehensive insights and course details, please visit Thierry de Meuus' website:",
           "<a href='https://www.t-de-meeus.fr/EnseignMeeus.html' target='_blank'>https://www.t-de-meeus.fr/EnseignMeeus.html</a>",
           sep = "<br/>"
+        ),
+        paste("To utilize this application, begin by importing your data in the 
+              Data Import and Filtering tab. Once your data is loaded, you can proceed to execute any of the analyses available within the application.",
+              sep = "<br/>"
         )
       ),
-      linebreaks(10),
       HTML(
+        "<h3>The data structure should be as follows:</h3>",
+        "<p style='margin-left: 20px;'>- One column for the Population identifier.</p>",
+        "<p style='margin-left: 20px;'>- The genetic marker can be encoded using the following patterns:</p>",
+        "<p style='margin-left: 20px;'>- The separator could be any symbol, here it is separated by  \"<span style='color: orange;'>/</span>\". </p>",
+        "<div style='display: flex;'>",
+        "<div style='flex: 1; display: flex; flex-direction: column;'>",  
+        data_one_col %>%
+          kable("html", align = 'l', caption = 'Genotype coded with one column.') %>%
+          kable_styling(full_width = F, position = "c") %>%
+          column_spec(column = 2, background = "#347893") %>%
+          column_spec(column = 3, background = "#34937E"),
+        "</div>",
+        
+        "<div style='flex: 1; display: flex; flex-direction: column;'>",  
+        data_two_col %>%
+          kable("html", align = 'l', caption = 'Genotype coded with two columns.',
+                col.names = c('Population', 'B12', 'B12', 'C07', 'C07')) %>%
+          kable_styling(full_width = F, position = "c") %>%
+          column_spec(column = 2, background = "#347893") %>%
+          column_spec(column = 3, background = "#699CB3") %>%
+          column_spec(column = 4, background = "#34937E") %>%
+          column_spec(column = 5, background = "#5AB7AB"),
+        "</div>",
+        
+        "</div>",
+        "<br>",
+        "<p style='margin-left: 20px;'>- Optional two columns for the GPS coordinates.</p>",
+        "<div style='margin-left: 40px;'>",
+        data_gps %>%
+          kable("html", align = 'l', caption = "Data frame with GPS coordinates") %>%
+          kable_styling(full_width = F, position = "c") %>%
+          column_spec(column = 2, background = "#347893") %>%
+          column_spec(column = 3, background = "#699CB3"),
+        "</div>"
+      ),
+      HTML(
+        "<h3>Contact</h4>",
         "If you have any suggestions or encounter bugs, please don't hesitate to contact Vincent Manzanilla through the GitHub page:<br/><a href='https://github.com/vincentmanz/shiny_pop_gen' target='_blank'>https://github.com/vincentmanz/shiny_pop_gen</a>"
       )
     ),
@@ -58,12 +130,18 @@ body <- dashboardBody(
       icon = icon("cog", lib = "glyphicon")
     ),
     tabItem(
+      tabName = "general_stats",
+      general_stats_UI(),
+      icon = icon("cog", lib = "glyphicon")
+    ),
+    tabItem(
       tabName = "drift",
       generateGeneticDriftUI(),
       icon = icon("cog", lib = "glyphicon")
     )
   )
 )
+
 
 ui <- dashboardPage(
   skin = "midnight",
@@ -75,6 +153,7 @@ ui <- dashboardPage(
 server <- function(input, output, session) {
   # Combine server functions from other source files
   server_import_data(input, output, session)
+  general_stats_server(input, output, session)
   server_genetic_drift(input, output, session)
 }
 
