02a918115574afaf27804812676004dbd06e0686
vincentmanz <vincent.manzanilla@gmail.com>
2023-09-12 10:31:52 +0200

    temp

--- module/server_import_data.R
+++ module/server_import_data.R
@@ -4,48 +4,36 @@ source("www/helper.R")
 
 empty_data <- tibble( read.csv("https://www.t-de-meeus.fr/Enseign/BoophilusAdultsDataCattle.txt", header = TRUE, sep = "\t"))
 
-
 server_import_data <- function(input, output, session) {
   # Render the empty table using kable and kableExtra
-  output$empty_table <- renderUI({
-    HTML(kable(empty_data, format = "html") %>%
-           kable_styling(full_width = FALSE))
-  })
+  output$empty_table <- renderUI({HTML(kable(empty_data, format = "html") %>% kable_styling(full_width = FALSE))})
   
   # Define the reactive expression to hold the data frame
   df <- reactiveVal()
-  # Load default data when the button is clicked
-  observeEvent(input$load_default_data, {
-    df( read.csv("https://www.t-de-meeus.fr/Enseign/BoophilusAdultsDataCattle.txt", header = input$header, sep = input$sep))
+  
+  # Load default data when the button is clicked or handle file upload
+  observeEvent(input$load_default_data | input$file1, {
+    data_source <- if (input$load_default_data) {
+      "https://www.t-de-meeus.fr/Enseign/BoophilusAdultsDataCattle.txt"
+    } else {input$file1$datapath}
+    df(read.csv(data_source, header = input$header, sep = input$sep, quote = input$quote))
   })
-  # Handle file upload
-  observeEvent(input$file1, {req(input$file1)
-    df(read.csv(input$file1$datapath, header = input$header, sep = input$sep, quote = input$quote))
-  })
+  
   # Handle filtering of data
-  observeEvent(input$run_filter, {req(df())
-    missing_code <- as.numeric(input$missing_code)
+  observeEvent(input$run_filter, {req(df()) missing_code <- as.numeric(input$missing_code)
     # Excluding columns
     excluded_cols <- unlist(strsplit(input$exclude_cols, ","))
-    if (!is.null(excluded_cols) &&
-        length(excluded_cols) > 0 && any(excluded_cols != "")) {
-      df_filtered <- df() %>%
-        select(-any_of(excluded_cols))
+    if (!is.null(excluded_cols) && length(excluded_cols) > 0 && any(excluded_cols != "")) {
+      df_filtered <- df() %>% select(-any_of(excluded_cols))
       df(df_filtered)
     }
     # Excluding rows
     excluded_rows <- unlist(strsplit(input$exclude_rows, "[, ]+"))
     excluded_rows <- as.integer(excluded_rows[excluded_rows != ""])
     if (length(excluded_rows) > 0) {
-      df_filtered <- df() %>%
-        slice(-excluded_rows)
+      df_filtered <- df() %>% slice(-excluded_rows)
       df(df_filtered)
     }
-  })
-  
-  # Display the first 15 rows of the dataframe
-  output$contents <- renderTable({ req(df())
-    head(df(), n = 15)
   })
   
   # Update the select input choices
@@ -55,16 +43,12 @@ server_import_data <- function(input, output, session) {
     updateSelectInput(session, 'latitude_data',choices = c("select" = "", colnames(df_local)))
     updateSelectInput(session, 'longitude_data', choices = c("select" = "", colnames(df_local)))
   })
-  
+
   # Create a reactiveValues object to store the results
   result_data <- reactiveValues( number_pop = 0, number_indv = 0, number_marker = 0, number_missing = 0, number_missing_per = 0)
   # Create the table and the map
   observeEvent(input$run_assign, {
-    req(
-      input$pop_data,
-      input$col_ranges_data,
-      df()
-    )
+    req(input$pop_data, input$col_ranges_data, df())
     missing_code <- input$missing_code
     df_local <- df()
     
