a3e6b0ef7962eb520d4ba8c4d1bc6c81728b897b
vincentmanz <vincent.manzanilla@gmail.com>
2024-12-12 10:46:19 +0100

    check file de thierry

--- LD.R
+++ LD.R
@@ -27,8 +27,8 @@ loci <- c("B12", "C07", "D12", "D10", "A12", "C03")
 
 # Define loci and locus pairs
 loci_pairs <- combn(loci, 2, simplify = FALSE)
-n_simulations <- as.integer(100)
-workers <- 16
+n_simulations <- as.integer(1000)
+workers <- 60
 
 # Function to create contingency tables for each population without splitting haplotypes
 create_contingency_tables <- function(data, loci) {
